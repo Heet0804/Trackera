518acf2adc4ad3aed90a59b656aecb832c055454
Heet0804 <heet.lakhani24@sakec.ac.in>
2026-03-01 12:36:48 +0530

    Some changes Made

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$database   = "attendance_erp";
+$host     = 'sql110.infinityfree.net';
+$username = 'if0_41220044';
+$password = '8rX3f6nZjQ4CJ6B';
+$database = 'if0_41220044_trackera';
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-mysqli_set_charset($conn, "utf8mb4");
+mysqli_set_charset($conn, 'utf8');
 ?>
